3c7b939b8f711a3e37bb7628d87f43be40223c49
junhae01 <jh040101@g.shingu.ac.kr>
2023-03-24 11:07:35 +0900

    if 문과 for 문 사용

---
+++ My project/Assets/Scripts/0324/Test_005.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_005 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    Debug.Log(i);
+        //}
+
+        //for (int i = 0; i < 10; i += 2)
+        //{
+        //    Debug.Log(i);
+        //}
+
+        //for (int i = 3; i > 0; i--)
+        //{
+        //    Debug.Log(i);
+        //}
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Debug.Log(i);
+            }
+        }
+
+        Debug.Log("==================================");
+
+        int sum = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            sum += 1;
+        }
+        Debug.Log(sum);
+
+        Debug.Log("================±¸±¸´Ü===============");
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Debug.Log(i.ToString() + " X " + j.ToString() + " = " + (i * j).ToString());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
